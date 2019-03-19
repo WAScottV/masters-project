6a3911744229ff74cf2fc10154695874dbb59705
Alex Scott <williamascott@cedarville.edu>
2019-03-18 21:28:50 -0400

    cleaning up sql docker config

--- api/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t express-api-umd .
-docker run -p 3000:8080 -d --name express-api-umd express-api-umd
-# docker run -d -p 3307:3306 --name my-sql-umd -e MYSQL_ROOT_PASSWORD=password my-sql-umd
--- sql/build.sh
+++
@@ -1,5 +0,0 @@
-docker stop my-sql-umd
-docker rm my-sql-umd
-docker build -t my-sql-umd .
-docker run -d -p 3307:3306 --name my-sql-umd -e MYSQL_ROOT_PASSWORD=password my-sql-umd
-docker exec -it my-sql-umd bash -l
